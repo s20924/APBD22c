4658b29e5f46669faa4183f32769f2ad52d25c5e
Patryk Popiołek <s20924@pjwstk.edu.pl>
2022-03-06 12:51:29 +0100

    test

--- APBD1.1/APBD1.1/Program.cs
+++ APBD1.1/APBD1.1/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace APBD1._1
 {
     class Program
     {
-        static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1) throw new ArgumentNullException();
+            var websiteURL = args[0];
+            Console.Write(websiteURL);
+
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(websiteURL);
+            //Console.WriteLine(response);
+            var content = await response.Content.ReadAsStringAsync();
+            //Console.WriteLine(response);
+
+            var regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+
+            //var a = $"Content {content}";
+            //var b = @"\.-]";
+
+            var matchCollection = regex.Matches(content);
+
+            var set = new HashSet<string>();
+
+            foreach (var item in matchCollection)
+            {
+                set.Add(item.ToString());
+            }
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
+
